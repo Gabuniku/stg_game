c00f747531722188159ee756cf9ca12b8473e0ff
Gabuniku <67952831+Gabuniku@users.noreply.github.com>
2021-10-19 20:39:42 +0900

    Rectクラスを追加 各関数にドキュメントを追加

--- script/stg_util.js
+++ script/stg_util.js
@@ -5,7 +5,9 @@ import { ENEMIES, BULLETS } from "./game.js"
 
 export class Pos {
     /**
-     * 座標オブジェクト
+     * 座標を表すクラス
+     * @param {number} x 
+     * @param {number} y 
      */
     constructor(x = 0, y = 0) {
         this.x = x;
@@ -13,21 +15,50 @@ export class Pos {
     }
 }
 
+export class Rect {
+    /**
+     * 短形の当たり判定を表すクラス
+     * @param {Pos} pos 
+     * @param {number} width 
+     * @param {number} height 
+     */
+    constructor(pos = null, width = 0, height = 0) {
+        if (pos == null) {
+            pos = new Pos();
+        }
+        this.pos = pos;
+        this.width = width;
+        this.height = height;
+    }
+}
+/**
+ * minとmaxの間にxがあるかどうか返す
+ * @param {number} x 
+ * @param {number} min 
+ * @param {number} max 
+ * @returns bool
+ */
 export function between(x, min, max) {
     return x >= min && x <= max;
-  }
+}
 
+/**
+ * 二点の距離を計算
+ * @param {Pos} pos1 
+ * @param {Pos} pos2 
+ * @returns number
+ */
 export function calcDistance(pos1, pos2) {
-    /**
-     * 二点の距離を計算
-     */
     return Math.sqrt((pos1.x - pos2.x) ** 2 + (pos1.y - pos2.y) ** 2);
 }
 
+/**
+ * posに最も近いposをpos_arrayから見つける
+ * @param {Pos} pos 
+ * @param {Array<Pos>} pos_array 
+ * @returns  Pos or null
+ */
 export function getNearPos(pos, pos_array) {
-    /**
-     * posに最も近いposをpos_arrayから見つける
-     */
     let close_pos = null;
     let distance = 0;
     let most_min_distance = 0;
@@ -46,6 +77,11 @@ export function getNearPos(pos, pos_array) {
     return close_pos;
 }
 
+/**
+ * posから最も近い敵の位置を返す getNearPosのラッパー
+ * @param {Pos} pos 
+ * @returns Pos or null
+ */
 export function getNearEnemy(pos) {
     let poss = [];
     for (var i = 0; i < ENEMIES.length; i++) {
@@ -54,18 +90,25 @@ export function getNearEnemy(pos) {
     return getNearPos(pos, poss);
 }
 
+/**
+ * pos1からpos2への向きを計算
+ * @param {Pos} pos1 
+ * @param {Pos} pos2 
+ * @returns 
+ */
 export function getAngleToPos(pos1, pos2) {
-    /**
-     * pos1からpos2への向きを計算
-     */
     let rad = Math.atan2(pos2.y - pos1.y, pos2.x - pos1.x);
     return rad * (180 / Math.PI);
 }
 
+/**
+ * posからrangeに入ってるposを返す
+ * @param {Pos} pos 
+ * @param {Array<Pos>} pos_array 
+ * @param {number} range 
+ * @returns Pos
+ */
 export function getPosInRange(pos, pos_array, range) {
-    /**
-     * posからrangeに入ってるposを返す
-     */
     let hit_poss = [];
     let distance;
     let _pos;
@@ -79,18 +122,32 @@ export function getPosInRange(pos, pos_array, range) {
     return hit_poss;
 }
 
+/**
+ * posとsizeに一致する弾を取得
+ * is_enemyで弾種分別 true -> 敵弾
+ * @param {Pos} pos 
+ * @param {number} size 
+ * @param {Boolean} is_enemy 
+ * @returns Array<Bullet>
+ */
 export function getHitBullet(pos, size, is_enemy) {
-    /**
-     * posとsizeに一致する弾を取得
-     * is_enemyで弾種分別 true -> 敵弾
-     */
     let bullets = []
     for (let index = 0; index < BULLETS.length; index++) {
         const bullet = BULLETS[index];
-        const distance = calcDistance(pos,bullet.pos);
-        if(distance < size && bullet.is_enemy == is_enemy){
+        const distance = calcDistance(pos, bullet.pos);
+        if (distance < size && bullet.is_enemy == is_enemy) {
             bullets.push(bullet);
-        }        
+        }
     }
     return bullets;
+}
+
+/**
+ * min以上max以下の乱数を返す
+ * @param {number} min 
+ * @param {number} max 
+ * @returns number
+ */
+export function getRandomRange(min, max) {
+    return Math.random() * (max - min) + min;
 }
