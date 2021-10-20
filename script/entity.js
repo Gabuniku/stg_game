56078c5ad6a110bbaf4de5d8264f7752a7cc6c96
Gabuniku <67952831+Gabuniku@users.noreply.github.com>
2021-10-20 17:40:20 +0900

    rect属性を追加

--- script/entity.js
+++ script/entity.js
@@ -4,6 +4,12 @@ import { CANVAS_SIZE } from "./game.js"
 
 // 動くもののベースクラス
 export class Entity {
+    /**
+     * 
+     * @param {util.Pos} pos 
+     * @param {number} angle 
+     * @param {number} speed 
+     */
     constructor(pos, angle = 0, speed = 10) {
         this.pos = pos;
         this.width = this.hight = 10;
@@ -11,13 +17,16 @@ export class Entity {
         this.type;
         this.angle;
         this.rate_x;
-        this.size = 10;
         this.rate_y;
+        this.size = 10;
         this.canvas_size = CANVAS_SIZE;
         this.frame_cnt = 0;
         this.dead_flag = false;
         this.first_out = false;
         this.alpha = 1;
+        this.image = null;
+        this.hit_box = new util.Rect(this.pos.copy(),this.size, this.size);
+        this.show_box = new util.Rect(this.pos.copy(),this.size, this.size);
         this.set_angle(angle);
     }
     set_angle(angle) {
@@ -28,6 +37,8 @@ export class Entity {
     update() {
         this.frame_cnt++;
         this.move();
+        this.show_box.setCenter(this.pos);
+        this.hit_box.setCenter(this.pos);
         // 画面外
         if (this.pos.x < 0 - this.size || this.pos.x > this.canvas_size[0] + this.size ||
             this.pos.y < 0 - this.size || this.pos.y > this.canvas_size[1] + this.size) {
@@ -47,11 +58,19 @@ export class Entity {
         this.pos.x += this.speed * this.rate_x;
         this.pos.y += this.speed * this.rate_y;
     }
+    /**
+     * 
+     * @param {CanvasRenderingContext2D} ctx 
+     */
     render(ctx) {
         ctx.globalAlpha = this.alpha;
-        ctx.beginPath()
-        ctx.arc(this.pos.x, this.pos.y, this.size, 0, Math.PI * 2, true);
-        ctx.fill()
+        if (this.image instanceof Image) {
+            util.drawImageRect(this.image, this.show_box, ctx);
+        } else {
+            ctx.beginPath()
+            ctx.arc(this.pos.x, this.pos.y, this.size, 0, Math.PI * 2, true);
+            ctx.fill()
+        }
         ctx.globalAlpha = 1;
 
     }
